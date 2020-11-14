20a587a27816662b502e6273606a66c6bac0e9cd
Carlos Ramirez <carlosramir991@gmail.com>
2020-11-14 14:40:00 -0700

    Assignment 1

---
+++ Basic_C#_Programs/GitHub/Assignment1.cs/Assignment1.cs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignment1.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting System"+"\n"+
+                "Remember, we're \"accounting\" on you!");
+        }
+    }
+}
