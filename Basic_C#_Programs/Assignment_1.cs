68565293683d1c94702ea40a80aab5e0142361b5
Carlos Ramirez <carlosramir991@gmail.com>
2020-11-14 16:53:27 -0700

    commits

---
+++ Basic_C#_Programs/Assignment_1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assignment_1.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems"+"\n"+
+                "Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
